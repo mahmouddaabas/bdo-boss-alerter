dab235e78493363d531341b37a0f45728aa1ab68
Mahmoud Daabas <mahedaabas8@gmail.com>
2022-01-30 23:11:49 +0100

    Update data.js
    - Updated backend to also return boss images.

--- backend/data.js
+++ backend/data.js
@@ -20,24 +20,20 @@ async function get_boss_data(){
     //Get the next boss timer.
     let timer = $("body > main > div.default-box.text-center > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(2) > div.next-boss-timer.countdown").text();
 
-    //Check if array has 1 or 2 bosses.
-    /*if(current_boss.length == 3){
-        console.log(`The next bosses ${current_boss[1]} and ${current_boss[2]} are spawning in: ${timer}`)
-    }
-    else {
-        console.log(`The next boss ${current_boss[1]} is spawning in: ${timer}`)
-    }*/
-
-    //Convert timer to int.
-    //let timer_clean = timer.replaceAll(":", "");
-    //let timer_int = parseInt(timer_clean);
+    //Get boss icons.
+    let first_icon = "https://mmotimer.com"+ $("body > main > div.default-box.text-center > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(2) > img").attr("src");
+    let second_icon = "https://mmotimer.com"+ $("body > main > div.default-box.text-center > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(3) > img").attr("src");
 
     //Put the current_boss array and the int timer inside another array to return.
     let return_array = [];
     return_array.push(current_boss);
     return_array.push(timer);
+    return_array.push(first_icon.replace("..", ""));
+    return_array.push(second_icon.replace("..", ""));
     return return_array;
 }
+
+get_boss_data();
 
 module.exports = {
     get_boss_data
